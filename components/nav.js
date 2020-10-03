5396f7ccdc3afbd9e5f9f27926eae07f9d33d725
Patryk.Dyminski <patryk-dym@o2.pl>
2020-10-03 20:29:46 +0200

    Styled Nav component

--- components/nav.js
+++ components/nav.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useState } from "react";
 
 const localPages = [
   {href: '/', label: 'Dom'},
@@ -9,21 +10,42 @@ const localPages = [
 ]
 
 export default function Nav() {
+  const [showMe, setShowMe] = useState(false);
+  function toggle(){
+    setShowMe(!showMe);
+  }
+
   return (
-    <div> 
-      <header className="flex font-roboto text-5xl justify-center mt-8">
+    <div className="font-roboto"> 
+      <header className="flex text-2xl md:text-4xl justify-center text-center mt-8 mb-8">
         <h1>Kameralny Chór Politechniki Wrocławskiej</h1>
       </header>
+      <div className="p-4 cursor-pointer md:hidden" onClick={toggle}>
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+      </div>
       <nav>
-        <ul className="flex justify-center items-center p-8">
-            {localPages.map(({href, label}) => (
-              <li key={`${href}${label}`}>
-                <Link href={href}>
-                  <a className="btn-blue no-underline p-2 m-2">{label}</a>
-                </Link>
-              </li>
-            ))}
-        </ul>
+        <div className="hidden md:block">
+          <ul className="flex justify-evenly items-center pt-4 pb-4 border-4 border-black">
+              {localPages.map(({href, label}) => (
+                <li key={`${href}${label}`}>
+                  <Link href={href}>
+                    <a className="no-underline p-2 m-2 text-xl tracking-wider hover:text-gray-500">{label}</a>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className="md:hidden">
+          <ul className="pt-4 pb-4 text-center text-2xl bg-gray-100 rounded-lg" style={{display: showMe?"block":"none"}}>
+              {localPages.map(({href, label}) => (
+                <li key={`${href}${label}`} className="m-2">
+                  <Link href={href}>
+                    <a className="no-underline p-2 tracking-wider hover:text-gray-500">{label}</a>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
       </nav>
     </div>
   )
