9126c7d5f620f29b1c8d2a374dfc6743b7a17d23
Patryk.Dyminski <patryk-dym@o2.pl>
2020-11-19 13:32:39 +0100

    Redesigned footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,23 @@
+import Link from 'next/link'
+
 export default function Footer() {
     return (
       <footer className="border-t border-accent-2 bg-gray-500">
         <div className="container mx-auto px-5">
           <div className="py-28 flex flex-col lg:flex-row items-center">
-            <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-              KCHPWR
+            <div className="text-md lg:text-lg font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+              <Link href="https://www.facebook.com/KameralnyChorPWr" passHref={true}>
+                <h3 className="cursor-pointer hover:text-blue-600">https://www.facebook.com/KameralnyChorPWr</h3>
+              </Link>
+              <h3>kameralny@pwr.edu.pl</h3>
+            </div>
+            <h3 className="text-2xl lg:text-5xl font-bold tracking-tighter leading-tight text-center mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+              #KCHPWR
             </h3>
+            <div className="text-md lg:text-lg font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+              <h3>Studenckie Centrum Kultury Muzycznej</h3>
+              <h3>ul. Powstańców Śląskich 137a</h3>
+            </div>
           </div>
         </div>
       </footer>
