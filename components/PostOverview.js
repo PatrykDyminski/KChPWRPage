fc2cae6fa33e19fe2c6b70f2882604ad39f2f09a
Patryk.Dyminski <patryk-dym@o2.pl>
2020-11-29 20:55:28 +0100

    post overview style change

--- components/PostOverview.js
+++ components/PostOverview.js
@@ -9,10 +9,10 @@ export default function PostOverview({
   slug,
 }) {
   return (
-    <div key={slug} className="max-w-sm rounded overflow-hidden shadow-lg my-2 cursor-pointer">
+    <div key={slug} className="max-w-lg rounded overflow-hidden shadow-lg my-2 cursor-pointer">
       <Link as={`/post/${slug}`} href="/post/[slug]">
         <div>
-          <Image alt={title} src={image} width={400} height={225} />
+          <Image alt={title} src={image} width={600} height={337} />
           <div className="px-6 py-4">
             <h3 className="font-bold text-xl mb-2 hover:underline">{title}</h3>
             <p className="text-grey-darker text-base">{date}</p>
